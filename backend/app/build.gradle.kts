9bad62e2da85c4311480a762dfd4841a67d908b6
Joshua Fitzhugh <joshua.alanfitzhugh@gmail.com>
2025-10-26 15:23:54 -0400

    feat: added simple spaces integration and config based connection

--- backend/app/build.gradle.kts
+++ backend/app/build.gradle.kts
@@ -9,10 +9,12 @@ repositories {
 
 dependencies {
     implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    implementation(platform("software.amazon.awssdk:bom:2.36.2"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.postgresql:postgresql")
+    implementation("software.amazon.awssdk:s3")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
---
+++ backend/app/src/main/java/com/shihtzhugh/StartupSpacesWriteTask.java
@@ -0,0 +1,32 @@
+package com.shihtzhugh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+public class StartupSpacesWriteTask implements CommandLineRunner {
+
+    private final S3Client s3Client;
+
+    @Value("${spaces.bucket}")
+    private String bucket;
+
+    StartupSpacesWriteTask(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key("test.txt")
+                        .acl("public-read")
+                        .build(),
+                RequestBody.fromString("Hello, DigitalOcean Spaces!"));
+    }
+}
